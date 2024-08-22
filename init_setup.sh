f78b39d7d595b3db511ac8fdc4588231fddb7a4f
sid2983 <sid24000576@gmail.com>
2024-08-22 12:57:36 +0530

    ENv and package update

---
+++ init_setup.sh
@@ -0,0 +1,43 @@
+
+
+echo [$(date)] : "Starting init_setup.sh"
+
+
+#create a virtual environment
+
+echo [$(date)] : "Creating virtual environment"
+
+conda create --prefix ./env python=3.11 -y
+
+echo [$(date)] : "Virtual environment created"
+
+# Activate the virtual environment
+
+echo [$(date)] : "Activating virtual environment"
+
+#activate the virtual environment and return the status code of the activation
+
+
+source activate ./env 
+
+echo [$(date)] : "Virtual environment activated "
+
+conda init && conda activate ./env &&  echo "Terminal activated" || echo "Terminal activation failed"
+
+echo [$(date)] : "Virtual environment activated in terminal successfully"
+
+
+
+
+
+# Install the required packages
+
+echo [$(date)] : "Installing required packages"
+
+pip install -r requirements_dev.txt
+
+echo [$(date)] : "Packages installed"
+
+echo [$(date)] : "END"
+
+
