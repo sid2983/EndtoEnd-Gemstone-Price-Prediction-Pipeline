4dce76ce9afa6558a5ff8d490f2a365c2786fbdb
sid2983 <sid24000576@gmail.com>
2024-08-26 19:12:48 +0530

    dvc init

--- test.py
+++ test.py
@@ -1,36 +1,70 @@
 import sys
 import mlflow
-from src.logger.loggings import logging
+import os
+# from src.logger.loggings import logging
 
-logging.info("This is my first ml package testing")
+# logging.info("This is my first ml package testing")
 
 # print(loggings.t)
 
 # print(sys.exc_info())
 
-def calc(x,y,ops=None):
-    if ops=='add':
-        return x+y
-    elif ops=='sub':
-        return x-y
-    elif ops=='mul':
-        return x*y
-    elif ops=='div':
-        return x/y
-    else:
-        return "Invalid Operation"
+# def calc(x,y,ops=None):
+#     if ops=='add':
+#         return x+y
+#     elif ops=='sub':
+#         return x-y
+#     elif ops=='mul':
+#         return x*y
+#     elif ops=='div':
+#         return x/y
+#     else:
+#         return "Invalid Operation"
     
+
+import numpy as np
+import pandas as pd
+
+Data = {
+    'Name': ['Tom', 'nick', 'krish', 'jack'],
+    'Age': [20, 21, 19, 18],
+    'City': ['Hyderabad', 'Bangalore', 'Chennai', 'Mumbai'],
+    'Salary': [10000, 20000, 15000, 30000],
+    'Designation': ['Software Engineer', 'Data Scientist', 'Data Analyst', 'ML Engineer']
+}
+
+df = pd.DataFrame(Data)
+
+
+#create a function for this
+
+def save_to_csv(df, path):
+    df.to_csv(path, index=False)
+    return "Data Saved Successfully"
+
 
 
 if __name__=="__main__":
-    x=934
-    y=1868
-    with mlflow.start_run():
-        result = calc(x,y,ops='add')
-        mlflow.log_param("x",x) 
-        mlflow.log_param("y",y)
-        mlflow.log_param("operation","add")
-        mlflow.log_metric("result",result)
+    os.makedirs("tdata/raw", exist_ok=True)
+    path = "tdata/raw/data.csv"
+    
+    result = save_to_csv(df, path)
+    print(result)
 
-        print("Addition Result:",result)
+
+
+
+# if __name__=="__main__":
+    # x=934
+    # y=1868
+    # with mlflow.start_run():
+    #     result = calc(x,y,ops='add')
+    #     mlflow.log_param("x",x) 
+    #     mlflow.log_param("y",y)
+    #     mlflow.log_param("operation","add")
+    #     mlflow.log_metric("result",result)
+
+    #     print("Addition Result:",result)
+
+
 
